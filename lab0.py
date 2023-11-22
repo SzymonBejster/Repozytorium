9417bb91b11b3d394e67685ab56de0678ca043ba
Szymon <szymon.bejster@onet.pl>
2023-11-22 17:10:53 +0100

    zakończnie zadania 3

--- lab0.py
+++ lab0.py
@@ -1,17 +1,46 @@
-def poleprostokata(dlugosc, szerokosc):
-    return dlugosc * szerokosc
+def dodawanie(x, y):
+    return x + y
 
-def obwodprostokata(dlugosc, szerokosc):
-    return 2 * (dlugosc + szerokosc)
+def odejmowanie(x, y):
+    return x - y
+
+def mnozenie(x, y):
+    return x * y
+
+def dzielenie(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Nie można dzielić przez zero."
+
+def potegowanie(x, y):
+    return x ** y
 
 
-dlugosc_boku = float(input("Podaj długość boku prostokąta: "))
-szerokosc_boku = float(input("Podaj szerokość boku prostokąta: "))
+print("Wybierz operację:")
+print("1. Dodawanie")
+print("2. Odejmowanie")
+print("3. Mnożenie")
+print("4. Dzielenie")
+print("5. Potęgowanie")
 
 
-pole = poleprostokata(dlugosc_boku, szerokosc_boku)
-obwod = obwodprostokata(dlugosc_boku, szerokosc_boku)
+wybor = input("Wprowadź numer operacji (1/2/3/4/5): ")
 
 
-print(f"Pole prostokąta: {pole}")
-print(f"Obwód prostokąta: {obwod}")
+liczba1 = float(input("Podaj pierwszą liczbę: "))
+liczba2 = float(input("Podaj drugą liczbę: "))
+
+
+if wybor == '1':
+    print(f"Wynik dodawania: {dodawanie(liczba1, liczba2)}")
+elif wybor == '2':
+    print(f"Wynik odejmowania: {odejmowanie(liczba1, liczba2)}")
+elif wybor == '3':
+    print(f"Wynik mnożenia: {mnozenie(liczba1, liczba2)}")
+elif wybor == '4':
+    print(f"Wynik dzielenia: {dzielenie(liczba1, liczba2)}")
+elif wybor == '5':
+    print(f"Wynik potęgowania: {potegowanie(liczba1, liczba2)}")
+else:
+    print("Nieprawidłowy wybór. Wybierz numer od 1 do 5.")
